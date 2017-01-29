f203ff87e110a996b48198f55573b2500293d497
jverbosk <jverbosk@gmail.com>
2017-01-29 01:48:38 -0500

    updating scratch files

--- sandbox_test.rb
+++ sandbox_test.rb
@@ -1,16 +1,11 @@
 require "minitest/autorun"
-
-# Link test file and production file together
 require_relative "sandbox.rb"
 
-# Class to create a new instance for test
-class TestMinedMindsRandomPairs < Minitest::Test
+class TestSandbox < Minitest::Test
 
-  # Test 1 - Verify that array_test() will accept an array as a parameter
-  def test_1_array_parameter
-    test_array = ["a","b","c","d","e","f"]
-    #test_array = "test"  # non-string test to verify test fails
-    assert_equal(string_test(test_array),array_test(test_array))
+  def test_1_check_something
+    results = function()
+    assert_equal(true, results)
   end
 
 end
